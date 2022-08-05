a59254a25dd11ab37a241f174ba2fd671140fad6
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-05 23:46:41 +0900

    update: app.module.tsのimportにmoduleを追加

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TestModule } from './module/test.module'
+import { AuthModule } from './module/auth.module'
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, AuthModule],
 })
 export class AppModule {}
