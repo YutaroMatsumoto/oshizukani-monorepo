0485c7172351f1425cb744ff3838d6e2ba072f9b
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-05 23:47:44 +0900

    update: users.moduleとusers.serviceを追加

---
+++ be/src/service/users.service.ts
@@ -0,0 +1,18 @@
+import { Repository } from 'typeorm'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from 'src/entity/user.entity'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  // ユーザーを一人を返す
+  findOne(email: User['email']): Promise<User | undefined> {
+    // typeormからDBにアクセスして、ユーザーを取得する
+    return this.userRepository.findOne({ where: { email } })
+  }
+}
