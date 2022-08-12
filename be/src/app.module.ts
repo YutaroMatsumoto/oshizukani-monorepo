a9c3c80200eec48b967c62443e2c6a6386f7574a
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-12 13:41:02 +0900

    update: envファイルを読み込める用に実装（ormconfigはTODO）

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { TestModule } from 'src/module/test.module'
-import { AuthModule } from 'src/module/auth.module'
+// import { AuthModule } from 'src/module/auth.module'
 import { UsersModule } from 'src/module/users.module'
+import { CustomNamingStrategy } from 'src/config/db/customNamingStrategy'
+
+// TODO: ormconfigの内容を参照できるようにしたい
 
 @Module({
-  imports: [TestModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      isGlobal: true,
+    }),
+
+    // ---最低限動く設定--- //
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: '127.0.0.1', // TODO: envで環境を分ける。現在はlocal用の設定になっている。
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      entities: ['dist/entity/*.entity.{js,ts}'], // NOTE: なぜかsrcではなくdistにするとうまくいった
+      synchronize: process.env.NODE_ENV === 'development',
+      namingStrategy: new CustomNamingStrategy(),
+    }),
+    // ------------------ //
+    TestModule,
+    UsersModule,
+    // AuthModule,
+  ],
 })
 export class AppModule {}
--- be/src/config/db/ormconfig.ts
+++ be/src/config/db/ormconfig.ts
@@ -1,7 +1,6 @@
 import { DataSource } from 'typeorm'
 import { CustomNamingStrategy } from './customNamingStrategy'
 
-// TODO: dbの情報はenvファイルから取ってくる
 // TODO: srcもしくはdist直下どちらも見れるようにする？
 const source = new DataSource({
   type: 'postgres',
@@ -15,5 +14,22 @@ const source = new DataSource({
   synchronize: true, // TODO: 本番環境では推奨されていないため、環境によって分ける
   namingStrategy: new CustomNamingStrategy(),
 })
+
+// TODO: dbの情報はenvファイルから取ってくる。以下の設定にしたいがなぜかうまくいかない。envファイル読み込めてなさそう？
+// const source = new DataSource({
+//   type: 'postgres',
+//   host: process.env.DATABASE_HOST,
+//   port: 5432,
+//   // database: 'oshizukani-dev',
+//   // username: 'postgres',
+//   // password: 'postgres',
+//   database: process.env.DATABASE_NAME,
+//   username: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   entities: ['src/entity/*.{js,ts}'],
+//   migrations: ['src/migration/**/*.{js,ts}'],
+//   synchronize: process.env.NODE_ENV === 'development',
+//   namingStrategy: new CustomNamingStrategy(),
+// })
 
 export default source
