7909c851ad56f77c94b19532f34dbbb6e4a530fd
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-21 15:33:35 +0900

    update: ormconfigをアップデート

--- be/src/config/db/ormconfig.ts
+++ be/src/config/db/ormconfig.ts
@@ -1,8 +1,9 @@
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
+import { SeederOptions } from 'typeorm-extension'
 import { CustomNamingStrategy } from './customNamingStrategy'
 
 // TODO: srcもしくはdist直下どちらも見れるようにする？
-const source = new DataSource({
+const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: '127.0.0.1',
   port: 5432,
@@ -13,7 +14,25 @@ const source = new DataSource({
   migrations: ['src/migration/**/*.{js,ts}'],
   synchronize: true, // TODO: 本番環境では推奨されていないため、環境によって分ける
   namingStrategy: new CustomNamingStrategy(),
-})
+
+  seeds: ['src/seeding/seeds/**/*{.ts,.js}'],
+  factories: ['src/seeding/factories/**/*{.ts,.js}'],
+}
+
+const dataSource = new DataSource(options)
+
+// const source = new DataSource({
+//   type: 'postgres',
+//   host: '127.0.0.1',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'oshizukani-dev',
+//   entities: ['src/entity/*.{js,ts}'],
+//   migrations: ['src/migration/**/*.{js,ts}'],
+//   synchronize: true, // TODO: 本番環境では推奨されていないため、環境によって分ける
+//   namingStrategy: new CustomNamingStrategy(),
+// })
 
 // TODO: dbの情報はenvファイルから取ってくる。以下の設定にしたいがなぜかうまくいかない。envファイル読み込めてなさそう？
 // const source = new DataSource({
@@ -32,4 +51,4 @@ const source = new DataSource({
 //   namingStrategy: new CustomNamingStrategy(),
 // })
 
-export default source
+export default dataSource
