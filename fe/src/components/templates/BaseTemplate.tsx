c465884a3749f4c0e37b639da604661c31654636
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-29 23:27:57 +0900

    clean: feの環境を整理

--- fe/src/components/templates/BaseTemplate.tsx
+++ fe/src/components/templates/BaseTemplate.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-// import { Footer } from "src/components/organisms/Footer";
 import { Header } from 'src/components/organisms/Header'
-import styles from 'src/styles/Home.module.css'
-import Image from 'next/image'
 
+// TODO: Propsの見直し
 type Props = {
   test?: string
 }
@@ -12,19 +10,9 @@ export const BaseTemplate = ({ children }: React.PropsWithChildren<Props>) => {
   return (
     <div className="h-full bg-gray10">
       <Header />
-      <div className="pt-16 h-full">{children}</div>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className="pt-16 h-full flex justify-center items-center">
+        {children}
+      </div>
     </div>
   )
 }
