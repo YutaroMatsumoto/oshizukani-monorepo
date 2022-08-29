444b6b9df774e0d838fb6f433919ea439b9a83ad
YutaroMatsumoto <spurs03170629@gmail.com>
2022-08-29 23:26:19 +0900

    clean: feの環境を少し整理

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { PublicClientApplication } from '@azure/msal-browser'
-import { MsalProvider } from '@azure/msal-react'
-import { msalConfig } from 'src/config/authConfig'
-
-const pca = new PublicClientApplication(msalConfig)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MsalProvider instance={pca}>
+    <>
       <Head>
         <title>oshizukani</title>
       </Head>
       <Component {...pageProps} />
-    </MsalProvider>
+    </>
   )
 }
 
