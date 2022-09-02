b255ce1a07892e26d872d25864b4ae0dced7876f
YutaroMatsumoto <spurs03170629@gmail.com>
2022-09-02 23:35:49 +0900

    update: twin.macroを導入

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  // purge: [],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
       white: '#FFFFFF',
       black: '#000000',
-      green10: 'dcfce7',
-      green20: 'bbf7d0',
+      green10: '#dcfce7',
+      green20: '#bbf7d0',
       green30: '#86efac',
       green40: '#4ade80',
       green50: '#22c55e',
